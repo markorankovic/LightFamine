b03f807824fe9b2b9dded3816ed9e06dd9127a7f
Marko Rankovic <marko@markorankovic.com>
2020-08-15 17:17:20 +0100

    Initial Commit

---
+++ LightFamine/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  LightFamine
+//
+//  Created by Marko Rankovic on 8/15/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
