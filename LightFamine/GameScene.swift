8bd4e145b10db76edfd70e463326d812b6c573fb
Marko Rankovic <marko@markorankovic.com>
2020-08-17 00:24:23 +0100

    improved key movements

--- LightFamine/GameScene.swift
+++ LightFamine/GameScene.swift
@@ -1,9 +1,13 @@
 import SceneKit
 
-public class GameScene: SCNScene, SCNSceneRendererDelegate {
+public class GameScene: SCNScene, SCNPhysicsContactDelegate {
     
     var player: SCNNode? {
         rootNode.childNode(withName: "player", recursively: true)
+    }
+    
+    var exit: SCNNode? {
+        rootNode.childNode(withName: "exit", recursively: true)
     }
     
     var spotLights: [SCNNode] {
@@ -26,15 +30,33 @@ public class GameScene: SCNScene, SCNSceneRendererDelegate {
         }
     }
     
-    func evaluateBounds() {
-        print("Spot lights: \(spotLights.compactMap { $0.light })")
+    var playerHitExit: Bool {
+        guard let player = player, let exit = exit else {
+            return false
+        }
+        return physicsWorld.contactTestBetween(player.physicsBody!, exit.physicsBody!, options: nil).count > 0
     }
     
+    var playerInTheDark: Bool {
+        return spotLightsPlayerFallsUnder.count == 0
+    }
+    
+    func update(_ time: TimeInterval) {
+        player?.runAction(.moveBy(x: xSpeed, y: 0, z: zSpeed, duration: 0.1))
+        if playerInTheDark {
+            print("You lost!")
+            player?.position = .init(x: 0.13777577877044678, y: 0.7823623418807983, z: 3.761770725250244)
+        } else if playerHitExit {
+            print("You won!")
+            player?.position = .init(x: 0.13777577877044678, y: 0.7823623418807983, z: 3.761770725250244)
+        }
+    }
+    
+    let speed: CGFloat = 0.03
+    var zSpeed: CGFloat = 0
+    var xSpeed: CGFloat = 0
+    
     public func keyDown(with event: NSEvent) {
-        print(spotLightsPlayerFallsUnder.count)
-        let speed: CGFloat = 0.1
-        var zSpeed: CGFloat = 0
-        var xSpeed: CGFloat = 0
         if event.keyCode == 17 {
             zSpeed = -speed
         }
@@ -53,7 +75,21 @@ public class GameScene: SCNScene, SCNSceneRendererDelegate {
             player?.physicsBody?.angularVelocity = .init()
             player?.physicsBody?.velocity = .init()
         }
-        player?.runAction(.moveBy(x: xSpeed, y: 0, z: zSpeed, duration: 0.1))
+    }
+    
+    public func keyUp(with event: NSEvent) {
+        if event.keyCode == 17 {
+            zSpeed = 0
+        }
+        if event.keyCode == 5 {
+            zSpeed = 0
+        }
+        if event.keyCode == 3 {
+            xSpeed = 0
+        }
+        if event.keyCode == 4 {
+            xSpeed = 0
+        }
     }
         
 }
--- LightFamine/GameViewController.swift
+++ LightFamine/GameViewController.swift
@@ -8,7 +8,11 @@
 import SceneKit
 import QuartzCore
 
-class GameViewController: NSViewController {
+class GameViewController: NSViewController, SCNSceneRendererDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didSimulatePhysicsAtTime time: TimeInterval) {
+        ((view as? SCNView)?.scene as? GameScene)?.update(time)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +27,20 @@ class GameViewController: NSViewController {
         scnView.allowsCameraControl = true
         
         // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        //scnView.showsStatistics = true
         //scnView.debugOptions = SCNDebugOptions.showPhysicsShapes
+        
+        scnView.delegate = self
     }
         
     override func keyDown(with event: NSEvent) {
         let scnView = self.view as! SCNView
         (scnView.scene as? GameScene)?.keyDown(with: event)
     }
-            
+    
+    override func keyUp(with event: NSEvent) {
+        let scnView = self.view as! SCNView
+        (scnView.scene as? GameScene)?.keyUp(with: event)
+    }
+    
 }
